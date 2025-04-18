68d0fd2d5bbdccb8aed3b1d534fbd880f55e8b57
sukjaelee <freelsj0@gmail.com>
2025-04-19 07:53:22 +0900

    clear unnecessary code

--- src/user/model.rs
+++ src/user/model.rs
@@ -16,19 +16,3 @@ pub struct User {
     pub file_id: Option<String>,
     pub origin_file_name: Option<String>,
 }
-
-impl Default for User {
-    fn default() -> Self {
-        Self {
-            id: String::default(),
-            username: String::default(),
-            email: None,
-            created_by: None,
-            created_at: None,
-            modified_by: None,
-            modified_at: None,
-            file_id: None,
-            origin_file_name: None,
-        }
-    }
-}
